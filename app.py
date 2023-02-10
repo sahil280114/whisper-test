f2d1f22e30ad617931669a71d3d80c76d092a7c4
Sahil Chaudhary <sahil280114@gmail.com>
2023-02-10 17:56:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ from io import BytesIO
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
-    global model
+    global pipe
     
-    model = whisper.load_model("base")
+    pipe = whisper.load_model("base")
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
-    global model
+    global pipe
 
     # Parse out your arguments
     mp3BytesString = model_inputs.get('mp3BytesString', None)
